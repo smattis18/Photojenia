dfc0ec2abd1ab765923654846a9752bbe568424b
Kneve327 <kneve327@gmail.com>
2018-12-06 21:39:01 -0500

    fixing errors

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./controllers/routes/apiRoutes")(app);
+require("./controllers/routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "development") {
   syncOptions.force = true;
 }
 
